8636a75431992f3ff572813a33e452e3461bbc8e
mahian <mahian8112@gmail.com>
2024-06-13 10:34:16 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -23,74 +23,84 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              '/Users/mahian/StudioProjects/lab_test/logo.jpg',
-              width: 200,
-              height: 100,
-              fit: BoxFit.contain,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Sign In',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 22,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.25,
               ),
-            ),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            const SizedBox(height: 10),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+              Center(
+                child: Image.asset(
+                  '/Users/mahian/StudioProjects/lab_test/logo.jpg',
+                  width: screenWidth * 0.5,
+                  height: screenWidth * 0.25,
+                  fit: BoxFit.contain,
                 ),
-                minimumSize: const Size(double.infinity, 60),
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const DashboardScreen()),
-                );
-              },
-              child: const Text('Sign In', style: TextStyle(fontSize: 18)),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      foregroundColor:
-                          const Color.fromARGB(255, 183, 182, 181)),
-                  child: const Text('Forgot Password'),
+              const SizedBox(height: 20),
+              const Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 22,
                 ),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                      foregroundColor:
-                          const Color.fromARGB(255, 183, 182, 181)),
-                  child: const Text('Sign Up'),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
                 ),
-              ],
-            ),
-          ],
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const DashboardScreen()),
+                  );
+                },
+                child: const Text('Sign In', style: TextStyle(fontSize: 18)),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        foregroundColor:
+                            const Color.fromARGB(255, 183, 182, 181)),
+                    child: const Text('Forgot Password'),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        foregroundColor:
+                            const Color.fromARGB(255, 183, 182, 181)),
+                    child: const Text('Sign Up'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -102,6 +112,8 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard',
@@ -110,84 +122,93 @@ class DashboardScreen extends StatelessWidget {
         backgroundColor: Colors.red,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              '/Users/mahian/StudioProjects/lab_test/logo.jpg',
-              width: 200,
-              height: 100,
-              fit: BoxFit.contain,
-              alignment: Alignment.centerLeft,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Student',
-              style: TextStyle(
-                fontSize: 32,
-              ),
-            ),
-            ClipOval(
-              child: Image.asset(
-                '/Users/mahian/StudioProjects/lab_test/mahian.jpg',
-                width: 100,
-                height: 100,
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text('Sawron Mahian Jalil',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            const Text('213350',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Image.asset(
+                  '/Users/mahian/StudioProjects/lab_test/logo.jpg',
+                  width: screenWidth * 0.5,
+                  height: screenWidth * 0.25,
+                  fit: BoxFit.contain,
                 ),
-                minimumSize: const Size(double.infinity, 60),
               ),
-              onPressed: () {},
-              child: const Text('Show QR Code', style: TextStyle(fontSize: 18)),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+              const SizedBox(height: 10),
+              const Text(
+                'Student',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
                 ),
-                minimumSize: const Size(double.infinity, 60),
+                textAlign: TextAlign.center,
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProfileScreen()),
-                );
-              },
-              child: const Text('Show Profile', style: TextStyle(fontSize: 18)),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+              const SizedBox(height: 10),
+              ClipOval(
+                child: Image.asset(
+                  '/Users/mahian/StudioProjects/lab_test/mahian.jpg',
+                  width: screenWidth * 0.25,
+                  height: screenWidth * 0.25,
+                  fit: BoxFit.cover,
                 ),
-                minimumSize: const Size(double.infinity, 60),
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Sign Out', style: TextStyle(fontSize: 18)),
-            ),
-          ],
+              const SizedBox(height: 10),
+              const Text('Sawron Mahian Jalil',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+              const Text('213350',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                onPressed: () {},
+                child:
+                    const Text('Show QR Code', style: TextStyle(fontSize: 18)),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfileScreen()),
+                  );
+                },
+                child:
+                    const Text('Show Profile', style: TextStyle(fontSize: 18)),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Sign Out', style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -199,6 +220,8 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -210,86 +233,88 @@ class ProfileScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                ClipOval(
-                  child: Image.asset(
-                    '/Users/mahian/StudioProjects/lab_test/mahian.jpg',
-                    width: 100, // Set appropriate width
-                    height: 100, // Set appropriate height
-                    fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  ClipOval(
+                    child: Image.asset(
+                      '/Users/mahian/StudioProjects/lab_test/mahian.jpg',
+                      width: screenWidth * 0.25,
+                      height: screenWidth * 0.25,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sawron Mahian Jalil',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '213350',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Phone',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: UnderlineInputBorder(),
+                  const SizedBox(width: 10),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sawron Mahian Jalil',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '213350',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 10),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Address',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Phone',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(),
                 ),
-                focusedBorder: UnderlineInputBorder(),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+              const SizedBox(height: 10),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: 'Address',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(),
                 ),
-                minimumSize: const Size(double.infinity, 60),
               ),
-              onPressed: () {},
-              child: const Text('Update', style: TextStyle(fontSize: 18)),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
                 ),
-                minimumSize: const Size(double.infinity, 60),
+                onPressed: () {},
+                child: const Text('Update', style: TextStyle(fontSize: 18)),
               ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Close', style: TextStyle(fontSize: 18)),
-            ),
-          ],
+              const SizedBox(height: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Close', style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
         ),
       ),
     );
